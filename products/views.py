59e71bd32f29aea261924e63160ca61b3d305bff
CuencaOscar <ocuencamoreno@gmail.com>
2024-03-26 23:16:16 -0500

    Add product listing and detail API views with URLs

--- products/views.py
+++ products/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def products_list(request):
+    """
+    List all products.
+    """
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    """
+    Retorna un producto específico por su pk o un 404 si no se encuentra.
+    """
+    try:
+        product = Product.objects.get(pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
